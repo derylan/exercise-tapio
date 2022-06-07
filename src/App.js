d76f2bcf6c840f432e2dc1002b7f038fe99f9ae1
Dericioso <dery.lantigua@gmail.com>
2022-06-07 11:39:28 +0200

    Create file to create new post, where local storage is configure and new a handle function that will work with the form to add post

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { Header, Footer } from './components';
 
 function App() {
+  
   return (
     <div className="App">
       <Header />
---
+++ src/components/AddPost.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import { useState, useRef, useEffect } from 'react'
+import { v4 as uuidv4} from 'uuid'
+
+const AddPost = () => {
+
+    const userName = useRef();
+    const title = useRef();
+    const context = useRef();
+    const LOCAL_STORAGE_KEY = 'newPost';
+    const [posts, setPosts] = useState([]);
+    const id = uuidv4();
+
+    useEffect(() => {
+        const storedPost = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+        if (storedPost) {
+            setPosts(storedPost)
+        }
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(posts));
+    }, [posts])
+
+    const handlePost = (e) => {
+        e.preventDefault();
+        let firstInput = userName.current.value;
+        let secondInput = title.current.value;
+        let thirdInput = context.current.value;
+        setPosts(prevPost => {
+            return[...prevPost, {id: id, firstInput, secondInput, thirdInput}]
+        })
+        firstInput.current.value = '';
+        secondInput.current.value = '';
+        thirdInput.current.value = '';
+    }
+
+    return (
+        <div>
+            <h2>Share some thoughts</h2>
+        </div>
+    )
+}   
+
+    export default AddPost  
