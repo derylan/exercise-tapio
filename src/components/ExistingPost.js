16c63f22bc5d5e4bddfeafa3096776a1975fb752
Dericioso <dery.lantigua@gmail.com>
2022-06-08 14:12:22 +0200

    Nothing really, sorry

---
+++ src/components/ExistingPost.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ExistingPost = () => {
+    
+    const API_URL = 'https://jsonplaceholder.typicode.com/todos/1';
+    
+    return (
+        <div>ExistingPost</div>
+    )
+}
+
+export default ExistingPost
